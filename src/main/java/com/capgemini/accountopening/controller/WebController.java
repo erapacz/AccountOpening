5e28ec42299a07683be9a2c58974ef4e218c7f75
Posthauer <jordan.posthauer@capgemini.com>
2018-08-10 15:29:48 -0500

    Finished Personal Details Page

--- src/main/java/com/capgemini/accountopening/controller/WebController.java
+++ src/main/java/com/capgemini/accountopening/controller/WebController.java
@@ -3,26 +3,26 @@ package com.capgemini.accountopening.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.capgemini.accountopening.model.PersonalDetails;
-import com.capgemini.accountopening.repository.PersonalDetailsRepository;
 
 @Controller
 public class WebController {
 	
 	List<PersonalDetails> personalDetails = new ArrayList<PersonalDetails>();
-	
-	@Autowired
-	private PersonalDetailsRepository personalDetailsRepo;
+
 	
 	@RequestMapping(value="/home", method=RequestMethod.GET)
     public String home(Model model) {
+        return "index";
+    }
+	
+	@RequestMapping(value="/home", method=RequestMethod.POST)
+    public String save(Model model) {
         return "index";
     }
 	
@@ -30,13 +30,6 @@ public class WebController {
     public String usererForm(Model model) {
         model.addAttribute("user", new PersonalDetails());
         return "personalInfoForm";
-    }
- 
-    @RequestMapping(value="/personalInfoForm", method=RequestMethod.POST)
-    public String usererSubmit(@ModelAttribute PersonalDetails personalDetails, Model model) {
-        model.addAttribute("user", personalDetails);
-        personalDetailsRepo.save(personalDetails);
-        return null;
     }
 
 }
