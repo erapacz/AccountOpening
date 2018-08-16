b15e7b7e010b8b2daef4196d95f33ff81ba447ef
atalathi <atalathi@LNAR-PF10291N.corp.capgemini.com>
2018-08-16 13:26:00 -0500

    Service And Service Implementation packages(unfinished)

--- src/main/java/com/capgemini/accountopening/repository/PersonalDetailsRepository.java
+++ src/main/java/com/capgemini/accountopening/repository/PersonalDetailsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.capgemini.accountopening.model.PersonalDetails;
 
+
 public interface PersonalDetailsRepository extends JpaRepository<PersonalDetails, Long> {
 
 }
