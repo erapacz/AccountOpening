98ed4227c935b082e994adcfe96d71d9cd90fea4
bphillips614 <bphillips614@gmail.com>
2018-08-21 12:03:37 -0500

    validation -Vinesh

--- src/main/resources/public/js/phone.js
+++ src/main/resources/public/js/phone.js
@@ -1,21 +1,16 @@
-/*//<script type="text/javascript">
+$(function() {
 
+    $('#phone').keyup(function(e) {
+        var numbers = this.value.replace(/\D/g, '');
+        // alert(numbers.length);
+        var specialChar = {
+            3 : '-',
+            6 : '-'
+        };
+        this.value = '';
+        for (var i = 0; i < numbers.length; i++) {
+            this.value += (specialChar[i] || '') + numbers[i];
+        }
+    })
 
-//create function 
-function($, .keyup))
-$('#phone').keyup(function(e) {
-	
-//	function phoneKey(event){
-	
-	var numbers = this.value.replace(/\D/g, '');
-	// alert(numbers.length);
-	var specialChar = {
-		3 : '-',
-		6 : '-'
-	};
-	this.value = '';
-	for (var i = 0; i < numbers.length; i++) {
-		this.value += (specialChar[i] || '') + numbers[i];
-	}
-})
-*/
+});
