afd5d5acc2c0d67d95d6897140580b6ba8916b48
Chu <raymond.chu@capgemini.com>
2018-08-23 13:57:28 -0500

    Added error html pages

--- src/main/java/com/capgemini/accountopening/controller/PageErrorController.java
+++ src/main/java/com/capgemini/accountopening/controller/PageErrorController.java
@@ -10,22 +10,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageErrorController implements ErrorController {
- 
+	 
 	@RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-	     
+        String errorType = "";
+        
 	    if (status != null) {
 	        Integer statusCode = Integer.valueOf(status.toString());
-	     
-	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return "error";
-	        }
-	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return "error";
+
+	        if(statusCode == HttpStatus.UNAUTHORIZED.value()) {
+	        	errorType = "error_401";
+	        }else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+	        	errorType = "error_500";
+	        }else if(statusCode == HttpStatus.NOT_FOUND.value()) {
+	        	errorType = "error_404";
+	        }else {
+	        	errorType = "error";
 	        }
 	    }
-        return "error";
+        return errorType;
     }
 
 	@Override
