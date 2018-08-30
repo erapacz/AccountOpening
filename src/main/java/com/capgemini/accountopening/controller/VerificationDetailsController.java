f47c4efbbf7b1d37afdb4c0b00745555ca9362b9
jposthau <jposthau@LNAR-6BJG7h2.corp.capgemini.com>
2018-08-30 12:21:10 -0500

    "Added rest template to VerificationDetailsController"

--- src/main/java/com/capgemini/accountopening/controller/VerificationDetailsController.java
+++ src/main/java/com/capgemini/accountopening/controller/VerificationDetailsController.java
@@ -1,12 +1,13 @@
 package com.capgemini.accountopening.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.accountopening.model.AccountDetails;
 import com.capgemini.accountopening.model.PersonalDetails;
+import com.capgemini.accountopening.model.VerificationDetails;
 import com.capgemini.accountopening.service.AccountDetailsServiceImplementation;
 import com.capgemini.accountopening.service.PersonalDetailsServiceImplementation;
 
@@ -19,30 +20,26 @@ public class VerificationDetailsController {
 	@Autowired
 	private PersonalDetailsServiceImplementation personalDetailsService;
 	
-	@PostMapping("/verify/account/{accNum}")
-	public AccountDetails getAccDetails(@PathVariable ("accNum") String accNum){
+	@GetMapping(value="/verify/account/{accNum}")
+	public VerificationDetails getAccDetails(@PathVariable ("accNum") String accNum){
 		AccountDetails account;
 		PersonalDetails personal;
+		VerificationDetails verify = new VerificationDetails();
+
 		try {
 			account = accountDetailsService.getAccountDetailsById(accNum);
 			personal = personalDetailsService.getPersonalDetailsById(account.getCustomerId());
+			verify.setAccNum(accNum);
+			verify.setDob(personal.getDob());
+			verify.setMaiden(personal.getMotherMName());
+			verify.setSsn(personal.getSsn());
 		} catch(Exception e) {
-			account = new AccountDetails();
-			account.setAccNumber(accNum);
+			verify.setAccNum(accNum);
+			verify.setDob(null);
+			verify.setMaiden(null);
+			verify.setSsn(null);
 		}
-		return account;
-		
+		return verify;
 	}
 	
-//	@PostMapping("/verify/personal/{custId}")
-//	public PersonalDetails getPerDetails(@PathVariable ("custId") Long custId){
-//		PersonalDetails person;
-//		try {
-//			person = personalDetailsService.getPersonalDetailsById(custId);
-//		} catch(Exception e) {
-//			person = new PersonalDetails();
-//			person.set(accNum);
-//		}
-//		return account;
-//	}
 }
---
+++ src/main/java/com/capgemini/accountopening/model/VerificationDetails.java
@@ -0,0 +1,55 @@
+package com.capgemini.accountopening.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class VerificationDetails {
+	
+	private String accNum;
+	
+	private String maiden;
+	
+	private String ssn;
+	
+	@DateTimeFormat(pattern ="yyyy-MM-dd")
+	private Date dob;
+
+	public String getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(String accNum) {
+		this.accNum = accNum;
+	}
+
+	public String getMaiden() {
+		return maiden;
+	}
+
+	public void setMaiden(String maiden) {
+		this.maiden = maiden;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	@Override
+	public String toString() {
+		return "VerificationDetails [accNum=" + accNum + ", maiden=" + maiden + ", ssn=" + ssn + ", dob=" + dob + "]";
+	}
+	
+}
